71dadea82f33ade9db509ac6cef3d47ed4c56728
DavidPrado <david.pradomejuto@iesmiguelherrero.com>
2024-02-20 19:46:50 +0100

    modificar request

--- app/Http/Requests/crearIncidenciaRequest.php
+++ app/Http/Requests/crearIncidenciaRequest.php
@@ -31,7 +31,7 @@ class CrearIncidenciaRequest extends FormRequest
             Portatil, Impresoras, Educantabria, Google Classroom, Dominio, Yedra, IesMiguelHerrero,
              WIECAN, Instalacion, Actualizacion',
             'descripcion' => 'max:256',
-            'fichero' => 'mimes:jpg,pdf,csv,xls,doc,docx,rtf,xlsx',
+            'fichero' => 'mimes:jpg,pdf,csv,rtf',
         ];
     }
 
@@ -48,6 +48,8 @@ class CrearIncidenciaRequest extends FormRequest
             'nombrePersona.max' => 'El campo nombre debe tener menos de 25 caracteres.',
             'apellido1.required' => 'El campo apellido1 es obligatorio.',
             'apellido2.required' => 'El campo apellido2 es obligatorio.',
+            'apellido1.required' => 'El campo apellido1 debe tener menos de 25 caracteres.',
+            'apellido2.required' => 'El campo apellido2 debe tener menos de 25 caracteres.',
             'nombreDepartamento.required' => 'El campo departamento es obligatorio.',
             'nombreDepartamento.max' => 'El campo departamento debe tener menos de 45 caracteres.',
             'tipoIncidencia.required' => 'El campo tipo de incidencia es obligatorio.',
