8c7679d930c54e757b74efd58363a3ccb4a634df
D0nP3ngu1n <efren.gutierrezcantero@iesmiguelherrero.com>
2024-02-21 19:58:16 +0100

    empezar con la vista de crear #43

--- app/Http/Controllers/IncidenciaController.php
+++ app/Http/Controllers/IncidenciaController.php
@@ -42,7 +42,7 @@ class IncidenciaController extends Controller
      */
     public function create()
     {
-    return view('incidencias.create');
+        return view('incidencias.create');
     }
 
     /**
