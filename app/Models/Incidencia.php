237f2a6b3fbeac1e263a354d4c17ddd2effa9354
Manuelo69 <ichigo2418@gmail.com>
2024-02-21 18:18:21 +0100

    Relaciones Fixes #53

--- app/Models/Incidencia.php
+++ app/Models/Incidencia.php
@@ -20,4 +20,44 @@ class Incidencia extends Model
     {
         return $this->belongsTo(IncidenciaSubtipo::class, 'subtipo_id', 'id');
     }
+
+    /**
+     * Relacion uno a muchos entre persona (creador) e incidencias
+     * @param null no recibe parametros
+     * @return
+     */
+    public function creador()
+    {
+        return $this->belongsTo(Persona::class, 'creador_id');
+    }
+
+    /**
+     * Relacion uno a muchos entre persona (responsable) e incidencias
+     * @param null no recibe parametros
+     * @return
+     */
+    public function responsable()
+    {
+        return $this->belongsTo(Persona::class, 'responsable_id');
+    }
+
+    /**
+     * Relacion uno a muchos entre incidencia y comentarios
+     * @param null no recibe parametros
+     * @return
+     */
+    public function comentarios()
+    {
+        return $this->hasMany(Comentario::class, 'incidencia_num');
+    }
+
+    /**
+     * Relacion uno a muchos entre equipo e incidencias
+     * @param null no recibe parametros
+     * @return
+     */
+    public function equipo()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_id');
+    }
 }
