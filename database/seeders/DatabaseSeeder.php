3968d8977e0c95b3b789378f0373c26d69da3608
Manuelo69 <ichigo2418@gmail.com>
2024-02-22 16:08:27 +0100

    asdads

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,17 +20,18 @@ class DatabaseSeeder extends Seeder
     {
         //Borrar las tablas en orden para que no haya errores en las claves foraneas
         DB::table('comentarios')->delete();
+        DB::table('incidencias_subtipos')->delete();
         DB::table('incidencias')->delete();
         DB::table('personal')->delete();
         DB::table('equipos')->delete();
         DB::table('aulas')->delete();
+        DB::table('incidencias_subtipos')->delete();
         DB::table('perfiles')->delete();
         DB::table('departamentos')->delete();
         //Rellenar las tablas en orden para evitar los errores de claves foraneas
         $this->call(AulaSeeder::class);
         $this->call(EquipoSeeder::class);
-        /*DB::table('incidencias_subtipos')->delete();
-        $this->call(IncidenciaSubtipoSeeder::class);*/
+        $this->call(IncidenciaSubtipoSeeder::class);
         $this->call(DepartamentoSeeder::class);
         $this->call(PersonaSeeder::class);
         $this->call(PerfilSeeder::class);
