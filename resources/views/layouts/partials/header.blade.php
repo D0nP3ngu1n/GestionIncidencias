b73804fdaff42fc250b56e665c6ba65215d6d149
TaniaEF <tania.echevarriafernandez@iesmiguelherrero.com>
2024-02-23 20:10:17 +0100

    Cambio de la forma de aplicar el estilo del nav y header. Fixes #89

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,19 +1,17 @@
 <header class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-    <!-- Topbar Search
-        <form
-            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <form class="navbar-search">
             <div class="input-group">
                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                     aria-label="Search" aria-describedby="basic-addon2">
                 <div class="input-group-append">
                     <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
+                        <i class="bi bi-search"></i>
                     </button>
                 </div>
             </div>
         </form>
-    -->
+
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
         <div class="topbar-divider d-none d-sm-block"></div>
@@ -22,7 +20,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                <img class="img-profile rounded-circle" src="assets/imagenes/perfil_01.jpg">
             </a>
         <!-- Dropdown - User Information
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <title>@yield('titulo')</title>
-    @vite('resources/sass/app.scss', 'resources/js/app.js')
+    @vite('resources/sass/app.scss', 'resources/js/app.js', 'resources/sass/_nav.scss')
     @vite('resources/css/navbar.css')
 </head>
 
