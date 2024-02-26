0d5006071cb26e2c95d3e857d0b3f6b9f2569d60
Manuelo69 <ichigo2418@gmail.com>
2024-02-26 18:46:53 +0100

    Export from view que he hecho sin querer desde la main, perdon jaja

--- app/Exports/IncidenciaExport.php
+++ app/Exports/IncidenciaExport.php
@@ -2,15 +2,20 @@
 
 namespace App\Exports;
 
+use App\Models\Incidencia;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
 
-class IncidenciaExport implements FromCollection
+class IncidenciaExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function view(): View
     {
-        //
+        return view('exports.invoices', [
+            'invoices' => Incidencia::all()
+        ]);
     }
 }
