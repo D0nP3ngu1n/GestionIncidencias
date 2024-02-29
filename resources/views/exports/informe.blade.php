98293179187c9a4c7772ab2b781dd0470c1f5c06
Manuelo69 <ichigo2418@gmail.com>
2024-02-29 20:14:32 +0100

    asadsdas

--- resources/views/exports/informe.blade.php
+++ resources/views/exports/informe.blade.php
@@ -1,5 +1,54 @@
-@forelse ($incidenciasAdmin as $incidenciaAdmin)
+<div>
+    <table>
+        @foreach ($incidenciasAdmin as $incidenciaAdmin)
+            <tr>
+                <td>{{ $incidenciasAdmin->id }}</td>
+                <td>{{ $incidenciasAdmin->tipo }}</td>
+                <td>{{ $incidenciasAdmin->subtipo_id }}</td>
+                <td>{{ $incidenciasAdmin->fecha_creacion }}</td>
+                <td>{{ $incidenciasAdmin->fecha_cierre }}</td>
+                <td>{{ $incidenciasAdmin->descripcion }}</td>
+                <td>{{ $incidenciasAdmin->estado }}</td>
+                <td>{{ $incidenciasAdmin->creador->nombre_completo }}</td>
+                <td>{{ $incidenciasAdmin->responsable->nombre_completo }}</td>
+                <td>{{ $incidenciasAdmin->duracion }}</td>
+                <td>{{ $incidenciasAdmin->prioridad }}</td>
+            </tr>
+        @endforeach
+    </table>
+    <table>
+        @foreach ($incidenciasUsuario as $incidenciaUsuario)
+            <tr>
+                <td>{{ $incidenciasUsuario->id }}</td>
+                <td>{{ $incidenciasUsuario->tipo }}</td>
+                <td>{{ $incidenciasUsuario->subtipo_id }}</td>
+                <td>{{ $incidenciasUsuario->fecha_creacion }}</td>
+                <td>{{ $incidenciasUsuario->fecha_cierre }}</td>
+                <td>{{ $incidenciasUsuario->descripcion }}</td>
+                <td>{{ $incidenciasUsuario->estado }}</td>
+                <td>{{ $incidenciasUsuario->creador->nombre_completo }}</td>
+                <td>{{ $incidenciasUsuario->responsable->nombre_completo }}</td>
+                <td>{{ $incidenciasUsuario->duracion }}</td>
+                <td>{{ $incidenciasUsuario->prioridad }}</td>
+            </tr>
+        @endforeach
+    </table>
+    <table>
+        <tr>
+            <th>Tipo</th>
+            <th>Total</th>
+        </tr>
+        @foreach ($estadisticasTipos as $estadisticaTipo)
+            <tr>
+                <td>{{ $estadisticaTipo->tipo }}</td>
+                <td>{{ $estadisticaTipo->total }}</td>
+            </tr>
+        @endforeach
+    </table>
+    <table>
 
-@empty
-
-@endforelse
+        @foreach ($tiempoDedicado as $tiempo)
+            <td></td>
+        @endforeach
+    </table>
+</div>
--- resources/views/incidencias/index.blade.php
+++ resources/views/incidencias/index.blade.php
@@ -20,6 +20,21 @@
                     <span>Crear Incidencia</span>
                 </a>
             </div>
+            <div class="col -2">
+                <a id="botonCrear" href="{{ route('export.informe.resueltas.admin') }}">
+                    <div class="svg-wrapper-1">
+                        <div class="svg-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2"
+                                stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24"
+                                fill="none" class="svg">
+                                <line y2="19" y1="5" x2="12" x1="12"></line>
+                                <line y2="12" y1="12" x2="19" x1="5"></line>
+                            </svg>
+                        </div>
+                    </div>
+                    <span>Exportar</span>
+                </a>
+            </div>
         </div>
     </div>
 
