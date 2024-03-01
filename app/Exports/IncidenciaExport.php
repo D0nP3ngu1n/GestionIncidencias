3f3caa736d4ec2c3f1b3b7e9b09e4e365dd93f48
Manuelo69 <ichigo2418@gmail.com>
2024-03-01 16:00:25 +0100

    Desplegable excel, pdf y csv Fixes #170

--- app/Exports/IncidenciaExport.php
+++ app/Exports/IncidenciaExport.php
@@ -3,19 +3,70 @@
 namespace App\Exports;
 
 use App\Models\Incidencia;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Collection;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 
-class IncidenciaExport implements FromCollection
+class IncidenciaExport implements FromCollection,WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
 
-        public function collection()
-        {
+     /*protected $data;
+
+    public function __construct($data = null)
+    {
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        if ($this->data instanceof Incidencia) {
+            return collect([$this->data]);
+        } else {
             return Incidencia::all();
         }
+    }*/
 
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        if ($this->data instanceof Incidencia) {
+            return collect([$this->data]);
+        } elseif ($this->data instanceof Collection) {
+            return $this->data;
+        } else {
+            return Incidencia::all();
+        }
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID Incidencia',
+            'Tipo',
+            'ID Subtipo',
+            'Fecha de creacion',
+            'Fecha de cierre',
+            'Descripcion',
+            'Estado',
+            'Url Adjunto',
+            'ID Creador',
+            'ID Responsable',
+            'Duracion',
+            'ID Equipo',
+            'Prioridad'
+
+        ];
+    }
 }
+
