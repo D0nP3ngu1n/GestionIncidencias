ff69fe11eee35f9212b9e886655d17fd27cdf74f
Manuelo69 <ichigo2418@gmail.com>
2024-03-01 15:11:31 +0100

    Continuación pdf, excel y csv Fixes #132

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -5,25 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Incidencia;
 use Illuminate\Http\Request;
 use App\Exports\IncidenciaExport;
+use App\Exports\informeExport;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
-use Barryvdh\DomPDF\Facade\Pdf;
 
 class ExportController extends Controller
 {
-    /**
-     * Vista del listado de incidencias a exportar
-     * @return mixed Devuelve la vista del listado de incidencias a exportar
-     */
     public function index()
     {
         $incidencias = Incidencia::all();
         return view('exports.index', ['incidencias' => $incidencias]);
     }
 
-    /**
-     * Exporta el listado de incidencias a excel
-     * @return mixed Realiza la exportacion a excel
-     */
     public function export(Request $request)
     {
         $incidencias = json_decode($request->input('incidencias'));
@@ -32,63 +25,182 @@ class ExportController extends Controller
         return Excel::download(new IncidenciaExport($incidencias), 'incidencias.xlsx');
     }
 
-    /**
-     * Exporta el listado de incidencias a pdf -> showpdf(show sin botones para la exportacion a pdf)
-     * @return mixed Realiza la exportacion a pdf
-     */
     public function exportpdf()
     {
-        $pdf = Pdf::loadView('exports.pdf', ['incidencias' => Incidencia::all()]);
-        return $pdf->download('incidencias.pdf');
+        //return Excel::download(new IncidenciaExport, 'incidencias.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
     }
 
-    /**
-     * Exporta el listado de incidencias a csv
-     * @return mixed Realiza la exportacion a csv
-     */
     public function exportcsv()
     {
-        return Excel::download(new IncidenciaExport(Incidencia::all()), 'incidencias.csv', \Maatwebsite\Excel\Excel::CSV);
+        //return Excel::download(new IncidenciaExport, 'incidencias.csv', \Maatwebsite\Excel\Excel::CSV);
     }
 
-    /**
-     * Vista de la incidencia a exportar
-     * @param Incidencia $incidencia objeto Incidencia
-     * @return mixed Devuelve la vista de la incidencia a exportar
-     */
+
+
     public function show(Incidencia $incidencia)
     {
         return view('exports.show', ['incidencia' => $incidencia]);
     }
 
-    /**
-     * Exporta el listado de incidencias a excel
-     * @param Incidencia $incidencia objeto Incidencia
-     * @return mixed Realiza la exportacion a excel de la incidencia
-     */
     public function exportInc(Incidencia $incidencia)
     {
         return Excel::download(new IncidenciaExport($incidencia), 'incidencia_' . $incidencia->id . '.xlsx');
     }
 
-    /**
-     * Exporta el listado de incidencias a pdf -> showpdf(show sin botones para la exportacion a pdf)
-     * @param Incidencia $incidencia objeto Incidencia
-     * @return mixed Realiza la exportacion a pdf de la incidencia
-     */
     public function exportpdfInc(Incidencia $incidencia)
     {
-        $pdf = Pdf::loadView('exports.showpdf', ['incidencia' => $incidencia]);
-        return $pdf->download('incidencia_' . $incidencia->id . '.pdf');
+        return Excel::download(new IncidenciaExport($incidencia), 'incidencia_' . $incidencia->id . '.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
     }
 
-    /**
-     * Exporta el listado de incidencias a csv
-     * @param Incidencia $incidencia objeto Incidencia
-     * @return mixed realiza la exportacion a csv de la incidencia
-     */
     public function exportcsvInc(Incidencia $incidencia)
     {
         return Excel::download(new IncidenciaExport($incidencia), 'incidencia_' . $incidencia->id . '.csv', \Maatwebsite\Excel\Excel::CSV);
     }
+
+    public function informeResueltasPorAdmin()
+    {
+        $incidenciasAdmin = Incidencia::where('estado', 'resuelta')
+            ->with('responsable')
+            ->get();
+
+        return Excel::download(InformeExport::resueltasPorAdministrador($incidenciasAdmin), 'informe_resueltas_admin.xlsx');
+    }
+
+    public function informeResueltasPorAdminCsv()
+    {
+        $incidenciasAdmin = Incidencia::where('estado', 'resuelta')
+            ->with('responsable')
+            ->get();
+
+        return Excel::download(InformeExport::resueltasPorAdministrador($incidenciasAdmin), 'informe_resueltas_admin.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+
+    public function informeResueltasPorAdminPdf()
+    {
+        $incidenciasAdmin = Incidencia::where('estado', 'resuelta')
+            ->with('responsable')
+            ->get();
+
+        return Excel::download(InformeExport::resueltasPorAdministrador($incidenciasAdmin), 'informe_resueltas_admin.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+    public function informeAbiertasPorUsuario()
+    {
+        $incidenciasUsuario = Incidencia::where('estado', 'abierta')
+            ->with('creador')
+            ->get();
+
+        return Excel::download(InformeExport::abiertasPorUsuario($incidenciasUsuario), 'informe_abiertas_usuario.xlsx');
+    }
+    public function informeAbiertasPorUsuarioCsv()
+    {
+        $incidenciasUsuario = Incidencia::where('estado', 'abierta')
+            ->with('creador')
+            ->get();
+
+        return Excel::download(InformeExport::abiertasPorUsuario($incidenciasUsuario), 'informe_abiertas_usuario.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+    public function informeAbiertasPorUsuarioPdf()
+    {
+        $incidenciasUsuario = Incidencia::where('estado', 'abierta')
+            ->with('creador')
+            ->get();
+
+        return Excel::download(InformeExport::abiertasPorUsuario($incidenciasUsuario), 'informe_abiertas_usuario.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+    public function informeEstadisticasTipos()
+    {
+        $incidencias = Incidencia::select('tipo', DB::raw('count(*) as total'));
+        $estadisticasTipos = InformeExport::estadisticasTipos($incidencias);
+
+        return Excel::download(new informeExport($estadisticasTipos), 'informe_estadisticas_tipos.xlsx');
+    }
+
+    public function informeEstadisticasTiposCsv()
+    {
+        $incidencias = Incidencia::select('tipo', DB::raw('count(*) as total'));
+        $estadisticasTipos = InformeExport::estadisticasTipos($incidencias);
+
+        return Excel::download(new informeExport($estadisticasTipos), 'informe_estadisticas_tipos.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+    public function informeEstadisticasTiposPdf()
+    {
+        $incidencias = Incidencia::select('tipo', DB::raw('count(*) as total'));
+        $estadisticasTipos = InformeExport::estadisticasTipos($incidencias);
+
+        return Excel::download(new informeExport($estadisticasTipos), 'informe_estadisticas_tipos.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+    public function informeTiempoDedicadoPorIncidencia()
+    {
+        $incidencias = Incidencia::all();
+        $tiempoDedicado = InformeExport::tiempoDedicadoPorIncidencia($incidencias);
+
+        return Excel::download(new InformeExport($tiempoDedicado), 'informe_tiempo_dedicado.xlsx');
+    }
+
+    public function informeTiempoDedicadoPorIncidenciaCsv()
+    {
+        $incidencias = Incidencia::all();
+        $tiempoDedicado = InformeExport::tiempoDedicadoPorIncidencia($incidencias);
+
+        return Excel::download(new InformeExport($tiempoDedicado), 'informe_tiempo_dedicado.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+
+    public function informeTiempoDedicadoPorIncidenciaPdf()
+    {
+        $incidencias = Incidencia::all();
+        $tiempoDedicado = InformeExport::tiempoDedicadoPorIncidencia($incidencias);
+
+        return Excel::download(new InformeExport($tiempoDedicado), 'informe_tiempo_dedicado.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+    public function informeTiemposResolucionPorTipo()
+    {
+        $incidencias = Incidencia::all();
+        $tiemposResolucion = InformeExport::tiemposResolucionPorTipo($incidencias);
+
+        return Excel::download(new InformeExport($tiemposResolucion), 'informe_tiempos_resolucion_por_tipo.xlsx', );
+    }
+    public function informeTiemposResolucionPorTipoCsv()
+    {
+        $incidencias = Incidencia::all();
+        $tiemposResolucion = InformeExport::tiemposResolucionPorTipo($incidencias);
+
+        return Excel::download(new InformeExport($tiemposResolucion), 'informe_tiempos_resolucion_por_tipo.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+    public function informeTiemposResolucionPorTipoPdf()
+    {
+        $incidencias = Incidencia::all();
+        $tiemposResolucion = InformeExport::tiemposResolucionPorTipo($incidencias);
+
+        return Excel::download(new InformeExport($tiemposResolucion), 'informe_tiempos_resolucion_por_tipo.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+    public function informeTiempoDedicadoEIncidenciasPorAdministrador()
+    {
+        $incidencias = Incidencia::all();
+        $informeCombinado = InformeExport::tiempoDedicadoEIncidenciasPorAdministrador($incidencias);
+
+        return Excel::download(new InformeExport($informeCombinado), 'informe_tiempo_dedicado_e_incidencias_por_admin.xlsx');
+    }
+
+    public function informeTiempoDedicadoEIncidenciasPorAdministradorCsv()
+    {
+        $incidencias = Incidencia::all();
+        $informeCombinado = InformeExport::tiempoDedicadoEIncidenciasPorAdministrador($incidencias);
+
+        return Excel::download(new InformeExport($informeCombinado), 'informe_tiempo_dedicado_e_incidencias_por_admin.csv', \Maatwebsite\Excel\Excel::CSV);
+    }
+
+    public function informeTiempoDedicadoEIncidenciasPorAdministradorPdf()
+    {
+        $incidencias = Incidencia::all();
+        $informeCombinado = InformeExport::tiempoDedicadoEIncidenciasPorAdministrador($incidencias);
+
+        return Excel::download(new InformeExport($informeCombinado), 'informe_tiempo_dedicado_e_incidencias_por_admin.pdf', \Maatwebsite\Excel\Excel::DOMPDF);
+    }
+
+
 }
