3a2e4805a4e3be6879faa62de3a278bd26f6bd05
Manuelo69 <ichigo2418@gmail.com>
2024-03-05 16:01:24 +0100

    Encabezados

--- resources/views/exports/resueltas.blade.php
+++ resources/views/exports/resueltas.blade.php
@@ -1,6 +1,21 @@
 
 <div>
     <table>
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Subtipo</th>
+                <th scope="col">Fecha creacion</th>
+                <th scope="col">Fecha Cierre</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Creador</th>
+                <th scope="col">Responsable</th>
+                <th scope="col">Duracion</th>
+                <th scope="col">Prioridad</th>
+            </tr>
+        </thead>
         @foreach ($incidencias as $incidencia)
             <tr>
                 <td>{{ $incidencia->id }}</td>
